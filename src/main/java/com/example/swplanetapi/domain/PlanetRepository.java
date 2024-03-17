94ed3d659f0a9b266b8e5b3e727fd57cbabc2472
John Enderson <johnenderson@outlook.com>
2024-03-17 17:49:00 -0300

    first commit

---
+++ src/main/java/com/example/swplanetapi/domain/PlanetRepository.java
@@ -0,0 +1,8 @@
+package com.example.swplanetapi.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlanetRepository extends CrudRepository<Planet,Long> {
+
+
+}
---
+++ src/main/java/com/example/swplanetapi/domain/PlanetService.java
@@ -0,0 +1,24 @@
+package com.example.swplanetapi.domain;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PlanetService {
+
+    private final PlanetRepository planetRepository;
+
+    public PlanetService(PlanetRepository planetRepository) {
+        this.planetRepository = planetRepository;
+    }
+
+    public Planet create(Planet planet) {
+        return planetRepository.save(planet);
+    }
+
+    public Optional<Planet> get(Long id) {
+        return planetRepository.findById(id);
+    }
+
+}
